1758c2755df74128d8d1da587e338d08a4128b81
hesto2 <hestermancarter@gmail.com>
2022-09-08 09:35:30 -0600

    add styles for demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
+import { createStyles, makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    container: {
+      background: "purple",
+    },
+  })
+);
 
 function App() {
-  return (
-  <div>
-    This is a test app
-  </div>
-  );
+  const classes = useStyles();
+  return <div>This is a test app</div>;
 }
 
 export default App;
